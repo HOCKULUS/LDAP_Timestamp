daa0c87cc2893e6201d64a906af0c0dd8ea42078
Tobias Heine <admin@miruth.de>
2022-02-21 11:47:01 +0100

    Update ldap_timestamp.ps1

--- ldap_timestamp.ps1
+++ ldap_timestamp.ps1
@@ -2,4 +2,4 @@ $date_1 = [DateTime] "01/01/1601 00:00:00"
 $date_2 = Get-Date #Your time zone!
 $ldap_timestamp_pre = ($date_2 - $date_1).TotalMilliseconds
 $ldap_timestamp = ([math]::Round($ldap_timestamp_pre)) * 10000
-$ldap_timestamp #Your time zone!
+Write-Host ($ldap_timestamp -as [decimal]) #Your time zone!
